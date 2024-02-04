e5ba559cfc4643f601c64bd649f803c503b9819b
Hardik29 <singhhardik14@gmail.com>
2024-02-05 02:29:06 +0530

    add

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ const frameMetadata = getFrameMetadata({
   image: 'https://drive.google.com/file/d/1c-zcMzH_5LOFElhYBq97LqOcmWhx56VV/view?usp=sharing',
 
   //which api to call when we click next button
-  post_url: 'https://meme-fram-znqt.vercel.app/api',
+  post_url: 'https://meme-fram-tugf.vercel.app/api',
 });
 
 export const metadata = {
