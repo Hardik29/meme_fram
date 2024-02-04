e1594937f944d5bf45495d188d5568cb9a0ecfe1
Hardik29 <singhhardik14@gmail.com>
2024-02-05 01:04:47 +0530

    gf

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ const frameMetadata = getFrameMetadata({
   image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4bE-m4Taohm0RAxy-6eFc2txRiYUnDNs96A&usqp=CAU',
 
   //which api to call when we click next button
-  post_url: 'https://meme-fram.vercel.app/api',
+  post_url: 'https://meme-fram-znqt.vercel.app/api',
 });
 
 export const metadata = {
